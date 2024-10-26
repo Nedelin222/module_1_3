205456e77a2d6129759b758de529cb8952534ecd
Nedelin222 <anedelin222@gmail.com>
2024-10-26 17:42:47 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,11 +1,11 @@
 name = 'Алексей'
-print('Name:',name)
+print('Name:', name)
 age = 32
-print('age:',age)
+print('age:', age)
 newage = 5
 age = (age + newage)
-print('new age:',age)
-Is_Student = (age > newage)
-print(Is_Student)
+print('new age:', age)
+is_student = True
+print('is_student:', is_student)
 
-
+# Здравствуйте Вячеслав, надеюсь щас правильно :)
